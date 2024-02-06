fd30a8dab2458edfa2fa069cee671a889845554e
orun sunny <sunny.mukut@gmail.com>
2024-02-06 22:52:32 +0600

    counter working

--- src/Task/TaskActions.jsx
+++ src/Task/TaskActions.jsx
@@ -1,7 +1,20 @@
-export default function TaskActions({ onAddClick, onDeleteAllClick }) {
+export default function TaskActions({ onAddClick, onDeleteAllClick, tasks }) {
+  const totalTasks = tasks.length;
+  console.log(totalTasks);
+  const completedTasks = tasks.filter(
+    (task) => task.status === "Completed"
+  ).length;
   return (
     <div className="mb-14 items-center justify-between sm:flex">
       <h2 className="text-2xl font-semibold max-sm:mb-4">Your Tasks</h2>
+      <div className="mb-4 text-white space-x-2">
+        <button className="rounded-md bg-yellow-500 px-3.5 py-2.5 text-sm font-semibold ">
+          Total Task: {totalTasks}
+        </button>
+        <button className="rounded-md bg-green-500 px-3.5  py-2.5 text-sm font-semibold ">
+          completed task: {completedTasks}
+        </button>
+      </div>
       <div className="flex items-center space-x-5">
         <button
           className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold"
--- src/Task/TaskList.jsx
+++ src/Task/TaskList.jsx
@@ -2,8 +2,16 @@ import { FaStar } from "react-icons/fa";
 import bgColor from "../utils/bgColor";
 
 export default function TaskList({ tasks, onEdit, onDelete, onFav }) {
+  // const totalTasks = tasks.length;
+  // const completedTasks = tasks.filter(
+  //   (task) => task.status === "Completed"
+  // ).length;
+
   return (
     <div className="overflow-auto">
+      <div className="mb-4 text-white">
+        {/* Total Tasks: {totalTasks}, Completed Tasks: {completedTasks} */}
+      </div>
       <table className="table-fixed  xl:w-full">
         <thead>
           <tr>
